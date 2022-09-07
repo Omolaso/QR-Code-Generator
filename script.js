bf4c44202e01c2e28d591312950b51bec4915e4b
omolaso <adextee001@gmail.com>
2022-09-07 03:31:12 +0100

    additions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const form = document.getElementById('submit');
 const generated = document.getElementById('generated-code');
 const spinner = document.getElementById('spinner');
+const download = document.getElementById('download');
+// const made = document.getElementById('made');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    clearUI();  // to clear previously generated code
 
     // to show spinner
     showSpinner();
@@ -12,14 +15,23 @@ form.addEventListener('submit', (e) => {
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
     // console.log(url, size);
-
     
     setTimeout(() =>{
-       hideSpinner();
-    }, 2000)
+       hideSpinner(); // hide spinner first
+
+       makeCode(url, size); // then generate code
+
+       setTimeout(() => {
+            const saveURL = generated.querySelector("img").src;
+            // console.log(saveURL);
+            downloadCode(saveURL);
+       }, 50); //setTimeout cause image link will not be available immediately.
+
+    }, 1000)
     
 });
 
+//  SPINNER
 
 const showSpinner = () => {
     spinner.style.display = 'block';
@@ -27,3 +39,40 @@ const showSpinner = () => {
 const hideSpinner = () => {
     spinner.style.display = 'none';
 }
+
+//  SPINNER
+
+// FROM CODE GENERATOR LIBRARY
+
+ const makeCode = (url, size) => {
+    const qrcode = new QRCode(generated, {
+        text: url,
+        width: size,
+        height: size,
+    });
+ }
+
+// FROM CODE GENERATOR LIBRARY
+
+//CLEAR UI
+
+const clearUI = () => {
+    generated.innerHTML = '';
+    download.innerHTML = '';
+}
+
+//CLEAR UI
+
+//DOWNLOAD BUTTON
+
+const downloadCode = (saveURL) => {
+    const downloadBtn = document.createElement('a');
+    downloadBtn.href = saveURL;
+    downloadBtn.download = 'qrcode'  // gave download attribute and attribute value automatically
+    downloadBtn.innerHTML = 'Download QR Code';
+    downloadBtn.id= 'btn'; // styled from style sheet
+    downloadBtn.classList = 'cursor-pointer p-2'
+    download.appendChild(downloadBtn)
+}
+
+//DOWNLOAD BUTTON
